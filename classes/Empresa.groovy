78ecc07b756f430ac0973acecafc4bda31baf146
Arthur A. <arthurakilalves@gmail.com>
2026-02-20 17:28:14 -0300

    feat: Adicionado Atrib: id, empresasCurtidas; Metds: printBonito

--- classes/Empresa.groovy
+++ classes/Empresa.groovy
@@ -1,6 +1,12 @@
 package LinketinderDesafioZG.classes
 
+import groovy.transform.ToString
+
+@ToString
 class Empresa {
+    private static int idContador = 0
+
+    int id = ++idContador
     String nome
     String emailCorporativo
     String cnpj
@@ -10,8 +16,27 @@ class Empresa {
     String descricaoEmpresa
 
     List<String> competenciasEsperadas = []
+    List<Integer> pessoasCurtidas = []
 
     void addCompetencia(String x){
         competenciasEsperadas << x.toLowerCase().trim()
     }
+
+    void printBonito(){
+        println("""Empresa:
+            Id: $id,
+            Nome: $nome,
+            Email: $emailCorporativo,
+            CNPJ: $cnpj,
+            País: $pais,
+            Estado: $estado,
+            CEP: $cep,
+            Descrição:
+            $descricaoEmpresa;
+            
+            Competências esparadas: ${competenciasEsperadas.join(", ")}
+            Id's de candidatos curtidos: ${pessoasCurtidas.join(", ")}
+            """)
+    }
+
 }
--- classes/Person.groovy
+++ classes/Person.groovy
@@ -1,6 +1,12 @@
 package LinketinderDesafioZG.classes
 
+import groovy.transform.ToString
+
+@ToString
 class Person {
+    private static int idContador = 0
+
+    int id = ++idContador
     String nome
     String email
     String cpf
@@ -10,8 +16,26 @@ class Person {
     String descricaoPessoal
 
     List<String> competencias = []
+    List<Integer> empresasCurtidas = []
 
     void addCompetencia(String x){
         competencias << x.toLowerCase().trim()
+    }
+
+    void printBonito(){
+        println("""Candidato:
+            Id: $id,
+            Nome: $nome,
+            Email: $email,
+            CPF: $cpf,
+            Idade: $idade,
+            Estado: $estado,
+            CEP: $cep,
+            Descrição:
+            $descricaoPessoal;
+            
+            Competências esparadas: ${competencias.join(", ")}
+            Id's de empresas curtidas: ${empresasCurtidas.join(", ")}
+            """)
     }
 }
